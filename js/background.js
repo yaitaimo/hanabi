56d7649fa5519adc58c01169db501399ed7aadc0
yaitaimo <yaitaimo@gmail.com>
2016-03-04 15:13:06 +0900

    fix bug.

--- js/background.js
+++ js/background.js
@@ -11,24 +11,21 @@
     var self = this;
     setInterval(function() {
       self.checkMergeNotifications();
-    }, this.interval);
+    }, self.interval);
     self.checkMergeNotifications();
   },
 
   checkMergeNotifications: function() {
-    this.notifications = [];
-    this.crawlNotifications();
-    if (this.notifications.length > 0) {
-      console.log("hanabi");
-    }
-    console.log("no_hanabi");
+    var self = this;
+    self.notifications = [];
+    self.crawlNotifications();
   },
 
   crawlNotifications: function() {
     var self = this;
-    $.get(this.notificationUrl(), function(data) {
+    $.get(self.notificationUrl(), function(data) {
       $(data).find('#notification-center .notifications .type-icon-state-merged').each(function() {
-        var $this = $(this).parents('li');
+        var $this = $(this).closest('li');
 
         var title = $this.find('.js-notification-target').text();
         var user = $this.find('.from-avatar:last-child').attr('alt');
@@ -40,6 +37,11 @@
           icon: icon
         });
       });
+      if (self.notifications.length > 0) {
+        console.log("hanabi");
+      } else {
+        console.log("no_hanabi");
+      }
     });
   },
 
