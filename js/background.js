cba4ab85cfa0db0d135431d18a23e98f8757c243
yaitaimo <yaitaimo@gmail.com>
2016-03-04 13:57:45 +0900

    Add background system.

--- js/background.js
+++ js/background.js
@@ -1,42 +1,49 @@
 ({
-    baseUrl: "https://github.com",
-    iterval: 1000 * 60 * 5,
-    notifications: [],
+  baseUrl: "https://github.com",
+  interval: 1000 * 10,
+  notifications: [],
 
-    init: function() {
-    },
+  init: function() {
+    this.startPolling();
+  },
 
-    startPolling: function() {
-        var self = this;
-        setInterval(function() {
-            self.checkMergeNotifications();
-        }, this.interval);
-    },
+  startPolling: function() {
+    var self = this;
+    setInterval(function() {
+      self.checkMergeNotifications();
+    }, this.interval);
+    self.checkMergeNotifications();
+  },
 
-    checkMergeNotifications: function() {
-        this.notifications = [];
-        this.crawlNotifications();
-        if (this.notifications.length() > 0) {
-            console.log("hanabi");
-        }
-    },
+  checkMergeNotifications: function() {
+    this.notifications = [];
+    this.crawlNotifications();
+    if (this.notifications.length > 0) {
+      console.log("hanabi");
+    }
+    console.log("no_hanabi");
+  },
 
-    crawlNotifications: function() {
-        var self = this;
-        $.get(this.notificationUrl(), function(data) {
-            $(data).find('#notification-center .notifications .type-icon-state-merged').each(function() {
-                var $this = $(this).parents('li');
+  crawlNotifications: function() {
+    var self = this;
+    $.get(this.notificationUrl(), function(data) {
+      $(data).find('#notification-center .notifications .type-icon-state-merged').each(function() {
+        var $this = $(this).parents('li');
 
-                var title = $this.find('.js-notification-target').text();
-                var user = $this.find('.from-avatar:last-child').attr('alt');
-                var icon = $this.find('.from-avatar:last-child').attr('src');
+        var title = $this.find('.js-notification-target').text();
+        var user = $this.find('.from-avatar:last-child').attr('alt');
+        var icon = $this.find('.from-avatar:last-child').attr('src');
 
-                self.notifications.push({
-                    title: title,
-                    user: user,
-                    icon: icon
-                )};
-            });
+        self.notifications.push({
+          title: title,
+          user: user,
+          icon: icon
         });
-    };
+      });
+    });
+  },
+
+  notificationUrl: function() {
+    return this.baseUrl + '/notifications';
+  }
 }).init();
